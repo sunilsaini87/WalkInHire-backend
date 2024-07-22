c1f6983f041c841ec0c1f83c5754b5e158b38a8c
Sunil Kumar Saini <sunilkumarsaini6363@gmail.com>
2024-07-22 14:10:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import express, { urlencoded } from "express";
+import connectDB from "./db/connection.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import bodyParser from "body-parser";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
+
+dotenv.config();
+// connect db
+connectDB();
+const PORT = process.env.PORT;
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: true, // Allow all origins
+    credentials: true, // Allow credentials (cookies, authorization headers) cross-origin
+  })
+); // Allows requests from all origins
+
+// api's route
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
+
+app.listen(PORT, () => {
+  console.log(`server running at port ${PORT}`);
+});
