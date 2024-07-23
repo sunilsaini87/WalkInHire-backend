e26f4f21199c5f936e801d4bb87ce504b5193542
Sunil Kumar Saini <sunilkumarsaini6363@gmail.com>
2024-07-23 15:01:03 +0530

    update files

--- models/company.model.js
+++ models/company.model.js
@@ -1,27 +1,30 @@
 import mongoose from "mongoose";
 
-const companySchema = new mongoose.Schema({
+const companySchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     description: {
-        type: String,
+      type: String,
     },
     website: {
-        type: String
+      type: String,
     },
     location: {
-        type: String
+      type: String,
     },
     logo: {
-        type: String
+      type: String,
     }, // URL to company logo
-    userId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User',
-        required:true
-    }
-}, { timestamps: true });
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 export const Company = mongoose.model("Company", companySchema);
--- routes/application.route.js
+++ routes/application.route.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { applyJob, getApplicants, getAppliedJobs, updateStatus } from "../controllers/application.controller.js";
+import {
+  applyJob,
+  getApplicants,
+  getAppliedJobs,
+  updateStatus,
+} from "../controllers/application.controller.js";
 import isAuthenticated from "../auth/isAuthenticated.js";
 const router = express.Router();
 
