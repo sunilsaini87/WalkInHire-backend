19f4d714058233b723822b81d2e03b50006b58d9
Sunil Kumar Saini <sunilkumarsaini6363@gmail.com>
2024-07-24 12:12:47 +0530

    update-x

--- auth/isAuthenticated.js
+++ auth/isAuthenticated.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+
 const isAuthenticated = async (req, res, next) => {
   try {
     const token = req.cookies.token;
@@ -8,17 +9,24 @@ const isAuthenticated = async (req, res, next) => {
         success: false,
       });
     }
-    const decode = await jwt.verify(token, process.env.SECRET_KEY);
+
+    const decode = jwt.verify(token, process.env.SECRET_KEY);
     if (!decode) {
       return res.status(401).json({
         message: "Invalid token",
         success: false,
       });
     }
+
     req.id = decode.userId;
     next();
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    return res.status(500).json({
+      message: "Internal server error",
+      success: false,
+    });
   }
 };
+
 export default isAuthenticated;
